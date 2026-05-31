87a0a33a92079def9b1a915fb5462e700efd4d1b
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-05-31 18:26:53 +0530

    Create theme_provider.dart

---
+++ lib/core/providers/theme_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeModeProvider =
+    StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
+  return ThemeModeNotifier();
+});
+
+class ThemeModeNotifier extends StateNotifier<ThemeMode> {
+  ThemeModeNotifier() : super(ThemeMode.dark) {
+    _loadTheme();
+  }
+
+  static const _key = 'theme_mode';
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getString(_key);
+    if (saved == 'light') {
+      state = ThemeMode.light;
+    } else {
+      state = ThemeMode.dark;
+    }
+  }
+
+  Future<void> toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (state == ThemeMode.dark) {
+      state = ThemeMode.light;
+      await prefs.setString(_key, 'light');
+    } else {
+      state = ThemeMode.dark;
+      await prefs.setString(_key, 'dark');
+    }
+  }
+}
