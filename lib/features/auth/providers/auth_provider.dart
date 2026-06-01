48e7d443477064bf1748806b8fca62cbd3fd083f
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-06-01 19:03:55 +0530

    Update auth_provider.dart

--- lib/features/auth/providers/auth_provider.dart
+++ lib/features/auth/providers/auth_provider.dart
@@ -1,17 +1,35 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../core/models/user_model.dart';
 import '../services/auth_service.dart';
 
 final authServiceProvider = Provider<AuthService>((ref) => AuthService());
 
 final authStateProvider = StreamProvider<UserModel?>((ref) async* {
-  final service = ref.read(authServiceProvider);
-  await for (final user in service.authStateChanges) {
+  await for (final user in FirebaseAuth.instance.authStateChanges()) {
     if (user == null) {
       yield null;
     } else {
-      final doc = await service._getUserModel(user);
-      yield doc;
+      try {
+        final doc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+        if (doc.exists) {
+          yield UserModel.fromMap({...doc.data()!, 'uid': user.uid});
+        } else {
+          yield UserModel(
+            uid: user.uid,
+            name: user.displayName ?? 'User',
+            email: user.email ?? '',
+            plan: 'free',
+            createdAt: DateTime.now(),
+          );
+        }
+      } catch (e) {
+        yield null;
+      }
     }
   }
 });
